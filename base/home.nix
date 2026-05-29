7a9e59f6425b98d4be9728de87832ea006bb6e25
wantJoy1 <wantjoy1@gmail.com>
2026-05-29 21:31:47 +0900

    home: add jujutsu (jj) version control

--- base/home.nix
+++ base/home.nix
@@ -29,6 +29,14 @@
     };
   };
 
+  programs.jujutsu = {
+    enable = true;
+    settings.user = {
+      name = "wantJoy1";
+      email = "wantjoy1@gmail.com";
+    };
+  };
+
   programs.gh = {
     enable = true;
     settings.git_protocol = "https";
